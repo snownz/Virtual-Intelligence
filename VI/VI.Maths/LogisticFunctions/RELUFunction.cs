45deac4676a28b6fe462f4bde089faaaae7f90e4
lucas <lucas_alfernandes@hotmail.com>
2017-11-05 18:12:13 -0800

    HotFix Logistic Functions

--- VI/VI.Maths/LogisticFunctions/RELUFunction.cs
+++ VI/VI.Maths/LogisticFunctions/RELUFunction.cs
@@ -6,17 +6,17 @@ namespace VI.Maths.LogisticFunctions
 {
     public static class RELUFunction
     {
-        public static void Derivative(Index t, ArrayView<float> v, float x)
+        public static void Derivative(Index t, ArrayView<float> v, ArrayView<float> x)
         {
             var p = t.X;
-            double y = Math.Max(0, x);
+            double y = Math.Max(0, x[p]);
             v[p] = (float)Convert.ToDouble(y > 0);
         }
 
-        public static void Function(Index t, ArrayView<float> v, float x)
+        public static void Function(Index t, ArrayView<float> v, ArrayView<float> x)
         {
             var p = t.X;
-            v[p] = Math.Max(0, x);
+            v[p] = Math.Max(0, x[p]);
         }
     }
 }
