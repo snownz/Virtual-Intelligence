7f97ccfc0b41e1682bc880659b926a16cc02dc33
Lucas de Almeida Fernandes <lucas_alfernandes@hotmail.com>
2017-11-21 08:23:16 -0200

    Update Program.cs

--- VI/VI.Labs/Program.cs
+++ VI/VI.Labs/Program.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using VI.Data;
 using VI.Neural.Node;
 using VI.Labs.Models;
 using VI.NumSharp;
-using System.Threading;
+using VI.Neural.LossFunction;
 
 namespace VI.Labs
 {
@@ -13,10 +12,9 @@ namespace VI.Labs
     {
         static void Main(string[] args)
         {
-            var values = new[] { .1f, .00f, 0f, 0f };
-
-            string header = $"Learning Rate: {values[0]}\nMin Error:{values[1]}\nMomentum: {values[3]}\n";
-
+            var rd = new Random();
+            var values = new[] {.1f, .002f };
+                        
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
             ProcessingDevice.Device = Device.CPU;
@@ -32,7 +30,7 @@ namespace VI.Labs
                 .WithMomentum(values[1])
                 .FullSynapse()
                 .Build();
-            
+
             var hiddens2 = new LayerBuilder(2, 2, values[0])
                 .Supervised()
                 .WithLeakRelu()
@@ -41,7 +39,7 @@ namespace VI.Labs
                 .WithMomentum(values[1])
                 .FullSynapse()
                 .Build();
-            
+
             var outputs = new LayerBuilder(2, 2, values[0])
                 .Supervised()
                 .WithSigmoid()
@@ -51,25 +49,78 @@ namespace VI.Labs
                 .FullSynapse()
                 .Build();
 
+            var loss = new SquareLossFunction();
+
             watch = System.Diagnostics.Stopwatch.StartNew();
             watch.Stop();
             Console.WriteLine($"Sinapse Time: {watch.ElapsedMilliseconds}ms");
 
+            var trainingValues = new[]
+            {
+                new[] {0f, 0f, 0f, 0f},
+                new[] {1f, 0f, 0f, 0f},
+                new[] {0f, 1f, 0f, 0f},
+                new[] {0f, 0f, 1f, 0f},
+                new[] {0f, 0f, 0f, 1f},
+                new[] {1f, 0f, 0f, 0f},
+                new[] {1f, 1f, 0f, 0f},
+                new[] {0f, 1f, 1f, 0f},
+                new[] {0f, 0f, 1f, 1f},
+                new[] {0f, 0f, 0f, 1f},
+                new[] {0f, 1f, 0f, 0f},
+                new[] {1f, 0f, 1f, 0f},
+                new[] {0f, 1f, 0f, 1f},
+                new[] {1f, 0f, 1f, 0f},
+                new[] {0f, 1f, 0f, 1f},
+                new[] {0f, 0f, 1f, 0f},
+                new[] {1f, 0f, 0f, 1f},
+                new[] {1f, 1f, 0f, 0f},
+                new[] {0f, 1f, 1f, 0f},
+                new[] {0f, 0f, 1f, 1f},
+                new[] {1f, 1f, 1f, 0f},
+                new[] {1f, 1f, 1f, 1f}
+            };
+
+            var desiredValues = new[]
+            {
+                new[] {1f, 0f},
+                new[] {1f, 0f},
+                new[] {1f, 0f},
+                new[] {1f, 0f},
+                new[] {0f, 1f},
+                new[] {1f, 0f},
+                new[] {1f, 0f},
+                new[] {1f, 0f},
+                new[] {0f, 1f},
+                new[] {0f, 1f},
+                new[] {1f, 0f},
+                new[] {1f, 0f},
+                new[] {0f, 1f},
+                new[] {1f, 0f},
+                new[] {0f, 1f},
+                new[] {1f, 0f},
+                new[] {0f, 1f},
+                new[] {1f, 0f},
+                new[] {1f, 0f},
+                new[] {0f, 1f},
+                new[] {1f, 0f},
+                new[] {0f, 1f}
+            };
+
             int cont = 0;
-
-            var t = EvenOrOddData("even", "odd");
-
+            int sizeTrain = 10;
+     
             var e = double.MaxValue;
 
             while (true)
             {
                 watch = System.Diagnostics.Stopwatch.StartNew();
                 e = 0;
-                foreach (var item in t)
+                for (int i = 0; i < sizeTrain; i++)
                 {
-                    var inputs = item.Values.Select(x => x.Value).ToArray();
-
-                    var desireds = item.DesiredValues.Select(x => x.Value).ToArray();
+                    var index = rd.Next(0, trainingValues.Length);
+                    var inputs = trainingValues[index];
+                    var desireds = desiredValues[index];
 
                     //watch = System.Diagnostics.Stopwatch.StartNew();
                     // Feed Forward
@@ -82,155 +133,11 @@ namespace VI.Labs
 
                     //watch = System.Diagnostics.Stopwatch.StartNew();
                     // Backward
-                    var _oe = ((ISupervisedLearning)outputs).Learn(_h2, desireds);
-                    var _he2 = ((ISupervisedLearning)hiddens2).Learn(_h, _oe);
+                    var _oe = ((ISupervisedLearning) outputs).Learn(_h2, desireds);
+                    var _he2 = ((ISupervisedLearning) hiddens2).Learn(_h, _oe);
                     ((ISupervisedLearning) hiddens).Learn(inputs, _he2);
                     //watch.Stop();
                     //Console.WriteLine($"\nBackward Time: { watch.ElapsedMilliseconds}ms");
 
                     // Error
-                    var e0 = Math.Abs(_o[0] - desireds[0]);
-                    var e1 = Math.Abs(_o[1] - desireds[1]);
-                    var error = Math.Sqrt(Math.Abs(e0 * e0 + e1 * e0));
-                    e += error / 2.0;
-                }
-                e /= t.Length;
-                cont++;
-                watch.Stop();
-                var time = watch.ElapsedMilliseconds;
-                Console.WriteLine($"Interactions: {cont}\nError: {e}\nTime: { time / (double)t.Length }ms");
-                Console.Title = $"TSPS (Training Sample per Second): {Math.Ceiling(1000d / ((double)time / (double)t.Length))}";
-            }
-        }
-
-        public static InputOutputTrainning[] EvenOrOddData(string even, string odd)
-        {
-            return new InputOutputTrainning[]
-            {
-                new InputOutputTrainning
-                {
-                    Values = new List<TrainningValues>
-                    {
-                        new TrainningValues { InputName = "i0", Value = 0 },
-                        new TrainningValues { InputName = "i1", Value = 0 },
-                        new TrainningValues { InputName = "i2", Value = 1 },
-                        new TrainningValues { InputName = "i3", Value = 0 },
-                    },
-                    DesiredValues = new List<Desired>
-                    {
-                        new Desired { Neuron = even, Value = 1 },
-                        new Desired { Neuron = odd, Value = 0 },
-                    }
-                },
-                new InputOutputTrainning
-                {
-                    Values = new List<TrainningValues>
-                    {
-                        new TrainningValues { InputName = "i0", Value = 1 },
-                        new TrainningValues { InputName = "i1", Value = 0 },
-                        new TrainningValues { InputName = "i2", Value = 0 },
-                        new TrainningValues { InputName = "i3", Value = 0 },
-                    },
-                    DesiredValues = new List<Desired>
-                    {
-                        new Desired { Neuron = even, Value = 1 },
-                        new Desired { Neuron = odd, Value = 0 },
-                    }
-                },
-                 new InputOutputTrainning
-                {
-                    Values = new List<TrainningValues>
-                    {
-                        new TrainningValues { InputName = "i0", Value = 0 },
-                        new TrainningValues { InputName = "i1", Value = 0 },
-                        new TrainningValues { InputName = "i2", Value = 0 },
-                        new TrainningValues { InputName = "i3", Value = 0 },
-                    },
-                    DesiredValues = new List<Desired>
-                    {
-                        new Desired { Neuron = even, Value = 1 },
-                        new Desired { Neuron = odd, Value = 0 },
-                    }
-                },
-                new InputOutputTrainning
-                {
-                    Values = new List<TrainningValues>
-                    {
-                        new TrainningValues { InputName = "i0", Value = 0 },
-                        new TrainningValues { InputName = "i1", Value = 1 },
-                        new TrainningValues { InputName = "i2", Value = 0 },
-                        new TrainningValues { InputName = "i3", Value = 0 },
-                    },
-                    DesiredValues = new List<Desired>
-                    {
-                        new Desired { Neuron = even, Value = 1 },
-                        new Desired { Neuron = odd, Value = 0 },
-                    }
-                },
-                new InputOutputTrainning
-                {
-                    Values = new List<TrainningValues>
-                    {
-                        new TrainningValues { InputName = "i0", Value = 0 },
-                        new TrainningValues { InputName = "i1", Value = 0 },
-                        new TrainningValues { InputName = "i2", Value = 0 },
-                        new TrainningValues { InputName = "i3", Value = 1 },
-                    },
-                    DesiredValues = new List<Desired>
-                    {
-                        new Desired { Neuron = even, Value = 0 },
-                        new Desired { Neuron = odd, Value = 1 },
-                    }
-                },
-
-                new InputOutputTrainning
-                {
-                    Values = new List<TrainningValues>
-                    {
-                        new TrainningValues { InputName = "i0", Value = 1 },
-                        new TrainningValues { InputName = "i1", Value = 0 },
-                        new TrainningValues { InputName = "i2", Value = 0 },
-                        new TrainningValues { InputName = "i3", Value = 1 },
-                    },
-                    DesiredValues = new List<Desired>
-                    {
-                        new Desired { Neuron = even, Value = 0 },
-                        new Desired { Neuron = odd, Value = 1 },
-                    }
-                },
-
-                new InputOutputTrainning
-                {
-                    Values = new List<TrainningValues>
-                    {
-                        new TrainningValues { InputName = "i0", Value = 0 },
-                        new TrainningValues { InputName = "i1", Value = 1 },
-                        new TrainningValues { InputName = "i2", Value = 0 },
-                        new TrainningValues { InputName = "i3", Value = 1 },
-                    },
-                    DesiredValues = new List<Desired>
-                    {
-                        new Desired { Neuron = even, Value = 0 },
-                        new Desired { Neuron = odd, Value = 1 },
-                    }
-                },
-
-                new InputOutputTrainning
-                {
-                    Values = new List<TrainningValues>
-                    {
-                        new TrainningValues { InputName = "i0", Value = 0 },
-                        new TrainningValues { InputName = "i1", Value = 0 },
-                        new TrainningValues { InputName = "i2", Value = 1 },
-                        new TrainningValues { InputName = "i3", Value = 1 },
-                    },
-                    DesiredValues = new List<Desired>
-                    {
-                        new Desired { Neuron = even, Value = 0 },
-                        new Desired { Neuron = odd, Value = 1 },
-                    }
-                },
-            };
-        }
-    }
-}
+ 
