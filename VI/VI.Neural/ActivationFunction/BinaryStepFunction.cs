f946b17f465fb06ee2bbed5680fe4b66f821a21c
lucas <lucas_alfernandes@hotmail.com>
2018-01-07 11:48:56 -0800

    Activation Functions

---
+++ VI/VI.Neural/ActivationFunction/BinaryStepFunction.cs
@@ -0,0 +1,17 @@
+﻿using VI.NumSharp.Arrays;
+
+namespace VI.Neural.ActivationFunction
+{
+    public class BinaryStepFunction : IActivationFunction
+    {
+        public Array<float> Activate(Array<float> v)
+        {
+            return (v >= 0);
+        }
+
+        public Array<float> Derivate(Array<float> v)
+        {
+            return (v >= 0);
+        }
+    }
+}
---
+++ VI/VI.Neural/ActivationFunction/ReluFunction.cs
@@ -0,0 +1,18 @@
+﻿using VI.NumSharp;
+using VI.NumSharp.Arrays;
+
+namespace VI.Neural.ActivationFunction
+{
+    public class ReluFunction : IActivationFunction
+    {
+        public Array<float> Activate(Array<float> v)
+        {
+            return NumMath.Max(.0f * v, v);
+        }
+
+        public Array<float> Derivate(Array<float> v)
+        {
+            return (v >= 0);
+        }
+    }
+}
